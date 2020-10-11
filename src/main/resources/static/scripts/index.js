409891b8de3c087c20ec6388a6748b6fd39e470d
PPFei5Zhou <pengfei_zhou555@163.com>
2020-10-11 15:08:26 +0800

    update index style

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,63 +1,76 @@
 $(function() {
-    layui.use(['form', 'layer', 'element'], function() {
-        let form = layui.form;
-        let layer = layui.layer;
+    layui.use(['form', 'layer', 'element', "jquery"], function() {
         let element = layui.element;
+        let $ = layui.$;
+        let form = layui.form;
 
         RenderUserInfo();
 
-        var active={
-            tabAdd:function(url,id,name){
-                element.tabAdd('contentnav',{
-                    title:name,
-                    content:'<iframe data-frameid="'+id+'" scrolling="auto" frameborder="0" src="'+url+'" style="width:100%;"></iframe>',
-                    id:id
-                });
-                iframeWH();
-            },
-            tabChange:function(id){
-                element.tabChange('contentnav',id);
-            },
-            tabDelete:function(id){
-                element.tabDelete('contentnav',id);
-            },
-            tabDeleteAll:function(ids){
-                $.each(ids,function(index,item){
-                    element.tabDelete('contentnav',item);
-                });
-            }
-        };
-
-        $(".site-url").on('click',function(){
-            var nav=$(this);
-            var length = $("ul.layui-tab-title li").length;
-            if(length<=0){
-                active.tabAdd(nav.attr("data-url"),nav.attr("data-id"),nav.attr("data-title"));
-            }else{
-                var isData=false;
-                $.each($("ul.layui-tab-title li"),function(){
-                    if($(this).attr("lay-id")==nav.attr("data-id")){
-                        isData=true;
+        // 配置tab实践在下面无法获取到菜单元素
+        $(".site-demo-active").on("click", function () {
+            var dataid = $(this);
+            //这时会判断右侧.layui-tab-title属性下的有lay-id属性的li的数目，即已经打开的tab项数目
+            if ($(".layui-tab-title li[lay-id]").length <= 0) {
+                //如果比零小，则直接打开新的tab项
+                active.tabAdd(
+                    dataid.attr("data-url"),
+                    dataid.attr("data-id"),
+                    dataid.attr("data-title")
+                );
+            } else {
+                //否则判断该tab项是否以及存在
+                var isData = false; //初始化一个标志，为false说明未打开该tab项 为true则说明已有
+                $.each($(".layui-tab-title li[lay-id]"), function () {
+                    //如果点击左侧菜单栏所传入的id 在右侧tab项中的lay-id属性可以找到，则说明该tab项已经打开
+                    if ($(this).attr("lay-id") == dataid.attr("data-id")) {
+                        isData = true;
                     }
                 });
-                if(isData==false){
-                    active.tabAdd(nav.attr("data-url"),nav.attr("data-id"),nav.attr("data-title"));
+                if (isData == false) {
+                    //标志为false 新增一个tab项
+                    active.tabAdd(
+                        dataid.attr("data-url"),
+                        dataid.attr("data-id"),
+                        dataid.attr("data-title")
+                    );
                 }
-                active.tabChange(nav.attr("data-id"));
             }
+            //最后不管是否新增tab，最后都转到要打开的选项页面上
+            active.tabChange(dataid.attr("data-id"));
         });
-
-        function iframeWH(){
-            var H = $(window).height()-80;
-            $("iframe").css("height",H+"px");
-        }
-
-        $(window).resize(function(){
-            iframeWH();
-        });
-
+        var active = {
+            //在这里给active绑定几项事件，后面可通过active调用这些事件
+            tabAdd: function (url, id, name) {
+                //新增一个Tab项 传入三个参数，分别对应其标题，tab页面的地址，还有一个规定的id，是标签中data-id的属性值
+                //关于tabAdd的方法所传入的参数可看layui的开发文档中基础方法部分
+                element.tabAdd("tabList", {
+                    title: name,
+                    content:
+                        '<iframe data-frameid="' +
+                        id +
+                        '" frameborder="0" src="' +
+                        url +
+                        '" style="width:100%;height:99%;"></iframe>',
+                    id: id, //规定好的id
+                });
+                FrameWH(); //计算ifram层的大小
+            },
+            tabChange: function (id) {
+                //切换到指定Tab项
+                element.tabChange("tabList", id); //根据传入的id传入到指定的tab项
+            },
+            tabDelete: function (id) {
+                element.tabDelete("tabList", id); //删除
+            },
+        };
+        //刷新界面 所有元素
         element.init();
         form.render();
+
+        function FrameWH() {
+            var h = $(window).height() - 128;
+            $("iframe").css("height", h + "px");
+        }
     });
 });
 
