45e4dda71e320981c6f28887c79beb6e06aae051
PPFei5Zhou <pengfei_zhou555@163.com>
2021-08-06 16:18:30 +0800

    delete useless import

--- src/main/java/com/easy/base/service/user/IRoleInfoService.java
+++ src/main/java/com/easy/base/service/user/IRoleInfoService.java
@@ -1,6 +1,5 @@
 package com.easy.base.service.user;
 
-import com.easy.base.domain.dao.user.RoleInfoDAO;
 import com.easy.base.domain.dto.user.RoleInfoDTO;
 import com.easy.base.service.IBaseService;
 
